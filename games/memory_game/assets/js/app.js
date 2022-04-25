391a4bdf80ad8a835c0de7092495499acc1aa9cb
Joan Preciado <js.preciadova@gmail.com>
2022-04-24 19:48:04 -0500

    Memory game
    Memory game complete and is show un main screen

--- games/memory_game/assets/js/app.js
+++ games/memory_game/assets/js/app.js
@@ -51,19 +51,82 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     ]
 
+    const grid          = document.querySelector('.grid');
+    const result        = document.querySelector('#result');
+    const restartBtn    = document.querySelector('.restart');
+    let cardsChoosen    = [];
+    let cardsChoosenId  = [];
+    let cardsWon        = [];
+    
+    // Restart game
+    restartBtn.addEventListener('click', restart);
+    
+    // Randomize cards
+    cardArray.sort( () => 0.5 -Math.random() );
+    
     // Create board
-    const grid = document.querySelector('.grid');
-
-
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
             let card = document.createElement('img');
-            card.setAttribute('src','assets/img/blank.png');
+            card.setAttribute('src','assets/img/blank.jpg');
             card.setAttribute('data-id', i);
-            //card.addEventListener('click', flipcard());
+            card.addEventListener('click', flipCard);
             grid.appendChild(card);
+        }
+
+        // Adjust height of images
+        let cards = document.querySelectorAll('img');
+        let width = cards[0].clientWidth;
+        cards.forEach( imgElem => {
+            imgElem.height = width;
+        });
+
+        setTimeout(() => {
+            let modal = document.querySelector('.modal');
+            modal.style.display = 'none';
+        }, 300);
+    }
+    createBoard();
+
+    // Check for matches
+    function checkForMatch() {
+        let cards = document.querySelectorAll('img');
+        const optionOneId = cardsChoosenId[0];
+        const optionTwoId = cardsChoosenId[1];
+        if (cardsChoosen[0] === cardsChoosen[1]) {
+            cards[optionOneId].setAttribute('src', 'assets/img/white.jpg');
+            cards[optionTwoId].setAttribute('src', 'assets/img/white.jpg');
+            cardsWon.push(cardsChoosen);
+        } else {
+            cards[optionOneId].setAttribute('src', 'assets/img/blank.jpg');
+            cards[optionTwoId].setAttribute('src', 'assets/img/blank.jpg');
+        }
+        cardsChoosen    = [];
+        cardsChoosenId  = [];
+        result.textContent = cardsWon.length * 100;
+        if (cardsWon.length === cardArray.length/2) {
+            let modal = document.querySelector('.modal');
+            let loader = document.querySelector('.loader');
+            let winnerImg = document.querySelector('.winner');
+            loader.style.display = 'none';
+            modal.style.display = 'flex';
+            winnerImg.style.display = 'block';
+            restartBtn.style.display = 'flex';
         }
     }
 
-    createBoard();
+    // Flip your card
+    function flipCard() {
+        let cardId = this.getAttribute('data-id');
+        cardsChoosen.push(cardArray[cardId].name);
+        cardsChoosenId.push(cardId);
+        this.setAttribute('src', cardArray[cardId].img);
+        if (cardsChoosen.length === 2) {
+            setTimeout(checkForMatch, 500);
+        }
+    }
+
+    function restart() {
+        location.reload();
+    }
 });
